713e6c4f211b838a927585c6f5508a6dea186887
Arnau Nin <ninbsnss92@gmail.com>
2025-03-17 22:18:07 +0100

    Modify vite.config.js

--- base-vite-project-sass/vite.config.js
+++ base-vite-project-sass/vite.config.js
@@ -1,5 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: "./", // Usa rutas relativas en los enlaces generados en el HTML
+  server: {
+    watch: {
+      usePolling: true, // Usa polling para detectar cambios
+    }
+  }
 });
